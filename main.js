b99222c78ef5222a4a50747c8128ddb067b5cb91
unknown <binnibja@gmail.com>
2021-11-08 22:00:20 +0000

    kokodkfsok

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 // TODO importa því sem nota þarf
 
+import { fetchNews } from './lib/news.js';
+import { el } from './lib/helpers.js';
+import { fetchAndRenderLists } from './lib/ui.js';
+
 /** Fjöldi frétta til að birta á forsíðu */
 const CATEGORY_ITEMS_ON_FRONTPAGE = 5;
-
 /** Vísun í <main> sem geymir allt efnið og við búum til element inn í */
-const main = document.querySelector('main');
-
+export const main = document.querySelector('main');
+const container = el('div')
+container.classList.add('newsList__list')
+main.appendChild(container)
 /**
  * Athugar útfrá url (`window.location`) hvað skal birta:
  * - `/` birtir yfirlit
  * - `/?category=X` birtir yfirlit fyrir flokk `X`
  */
 function route() {
+  fetchAndRenderLists(container, 5)
   // Athugum hvort það sé verið að biðja um category í URL, t.d.
   // /?category=menning
 
@@ -28,5 +34,16 @@ window.onpopstate = () => {
   // TODO útfæra
 };
 
+
+// 
+// async function prufa2(max) {
+//   const menning = await fetchNews('menning');
+//   for (let i = 0; i<max; i++) {
+//     const frettir = el('p', menning.items[i].title);
+//     frettir.classList.add('news_p')
+//     main.append(frettir);
+//   }
+// }
+// prufa2(10);
 // Í fyrsta skipti sem vefur er opnaður birtum við það sem beðið er um út frá URL
 route();
